1b9448cd851e7d90f27712f9081315644c956b5b
Evan <evan10takahashi@gmail.com>
2025-06-30 13:50:12 -0700

    Initial Commit

---
+++ TechnIQ/TechnIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnIQApp.swift
+//  TechnIQ
+//
+//  Created by Evan Takahashi on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TechnIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
